c626d304bb47b9b2dec1106fe9e50c3ed3ca7429
Jan Klenner <jan.klenner@gmx.de>
2018-11-12 15:45:53 +0100

    increase time

--- mysite/settings.py
+++ mysite/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
     }
 }
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config(conn_max_age=1500)
 DATABASES['default'].update(db_from_env)
 
 # Password validation
